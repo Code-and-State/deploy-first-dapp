b70534328ed605e8492bd0edd1e327214719098a
Sebastien Thuillier <sebastien.thuillier38@gmail.com>
2023-10-06 11:39:32 +0100

    Remove .dfx

--- .dfx/playground/canisters/demo/service.did.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-import type { ActorMethod } from '@dfinity/agent';
-
-export interface _SERVICE {
-  'changeMessage' : ActorMethod<[string], undefined>,
-  'seeMessage' : ActorMethod<[], string>,
-}
--- .dfx/playground/canisters/demo/service.did.js
+++
@@ -1,7 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  return IDL.Service({
-    'changeMessage' : IDL.Func([IDL.Text], [], []),
-    'seeMessage' : IDL.Func([], [IDL.Text], ['query']),
-  });
-};
-export const init = ({ IDL }) => { return []; };
